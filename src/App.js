f2356b0a5a56348343b9db3b42efd73b267e44c3
GrzegorzBanaszak <grzegorz.banaszak95@gmail.com>
2022-05-10 20:32:27 +0200

    Add default routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import "./scss/main.scss";
 import "./scss/reset.scss";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Form from "./components/Form";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import Logout from "./components/Logout";
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/oddaj-rzeczy" element={<Form />} />
+        <Route path="/logowanie" element={<Login />} />
+        <Route path="/rejestracja" element={<Register />} />
+        <Route path="/wylogowanie" element={<Logout />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
