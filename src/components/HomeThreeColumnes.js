a24859ac96b3114aced188ada0b54964f706d6f6
GrzegorzBanaszak <grzegorz.banaszak95@gmail.com>
2022-05-11 21:46:41 +0200

    Mobile version

--- src/components/HomeThreeColumnes.js
+++ src/components/HomeThreeColumnes.js
@@ -13,23 +13,23 @@ const HomeThreeColumnes = () => {
           <h5 className="three-columnes-subtitle">ODDANYCH WORKÓW</h5>
           <p className="three-columnes-description">
             Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel
-            enim a elit viverra elementuma. Aliquam erat volutpat.{" "}
+            enim a elit viverra elementuma. Aliquam erat volutpat.
           </p>
         </article>
         <article className="three-columnes-item">
-          <h3 className="three-columnes-title">10</h3>
-          <h5 className="three-columnes-subtitle">ODDANYCH WORKÓW</h5>
+          <h3 className="three-columnes-title">5</h3>
+          <h5 className="three-columnes-subtitle">WSPARTYCH ORGANIZACJI</h5>
           <p className="three-columnes-description">
             Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel
-            enim a elit viverra elementuma. Aliquam erat volutpat.{" "}
+            enim a elit viverra elementuma. Aliquam erat volutpat.
           </p>
         </article>
         <article className="three-columnes-item">
-          <h3 className="three-columnes-title">10</h3>
-          <h5 className="three-columnes-subtitle">ODDANYCH WORKÓW</h5>
+          <h3 className="three-columnes-title">7</h3>
+          <h5 className="three-columnes-subtitle">ZORGANIZOWANY ZBIÓREK</h5>
           <p className="three-columnes-description">
             Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel
-            enim a elit viverra elementuma. Aliquam erat volutpat.{" "}
+            enim a elit viverra elementuma. Aliquam erat volutpat.
           </p>
         </article>
       </div>
