8a0ff6f186c80f0b1db64f542f89611dd868b6fc
GrzegorzBanaszak <grzegorz.banaszak95@gmail.com>
2022-05-11 20:06:00 +0200

    Content for home section

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,8 @@
 import React from "react";
 import banner from "../assets/Home-Hero-Image.jpg";
 import Nav from "./Nav";
+import decoration from "../assets/Decoration.svg";
+import { Link } from "react-router-dom";
 const Home = () => {
   return (
     <>
@@ -11,6 +13,25 @@ const Home = () => {
         ></div>
         <div className="open-right">
           <Nav />
+          <div className="open-content">
+            <h2 className="content-title">Zacznij pomagać!</h2>
+            <h2 className="content-title">
+              Oddaj niechciane rzeczy w zaufane ręce
+            </h2>
+            <img
+              className="content-decoration"
+              src={decoration}
+              alt="decoration"
+            />
+            <div className="content-btns">
+              <Link className="content-btn" to="/oddaj-rzeczy">
+                Oddaj Rzeczy
+              </Link>
+              <Link className="content-btn" to="/oddaj-rzeczy">
+                Zorganizuj zbiórkę
+              </Link>
+            </div>
+          </div>
         </div>
       </section>
     </>
