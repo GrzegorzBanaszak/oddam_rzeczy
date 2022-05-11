16bf21708c01f6d42740f430c6cde9254b6a52b4
GrzegorzBanaszak <grzegorz.banaszak95@gmail.com>
2022-05-11 21:40:13 +0200

    Add content

---
+++ src/components/HomeThreeColumnes.js
@@ -0,0 +1,40 @@
+import React from "react";
+import bg from "../assets/columnes.png";
+const HomeThreeColumnes = () => {
+  return (
+    <section
+      style={{ backgroundImage: `url(${bg})` }}
+      id="three-columnes"
+      className="three-columnes"
+    >
+      <div className="three-columnes-container">
+        <article className="three-columnes-item">
+          <h3 className="three-columnes-title">10</h3>
+          <h5 className="three-columnes-subtitle">ODDANYCH WORKÓW</h5>
+          <p className="three-columnes-description">
+            Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel
+            enim a elit viverra elementuma. Aliquam erat volutpat.{" "}
+          </p>
+        </article>
+        <article className="three-columnes-item">
+          <h3 className="three-columnes-title">10</h3>
+          <h5 className="three-columnes-subtitle">ODDANYCH WORKÓW</h5>
+          <p className="three-columnes-description">
+            Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel
+            enim a elit viverra elementuma. Aliquam erat volutpat.{" "}
+          </p>
+        </article>
+        <article className="three-columnes-item">
+          <h3 className="three-columnes-title">10</h3>
+          <h5 className="three-columnes-subtitle">ODDANYCH WORKÓW</h5>
+          <p className="three-columnes-description">
+            Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel
+            enim a elit viverra elementuma. Aliquam erat volutpat.{" "}
+          </p>
+        </article>
+      </div>
+    </section>
+  );
+};
+
+export default HomeThreeColumnes;
