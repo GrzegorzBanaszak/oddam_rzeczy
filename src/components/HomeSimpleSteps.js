1ce1e9e783f695fee8468996fb66d08c4e927a4d
GrzegorzBanaszak <grzegorz.banaszak95@gmail.com>
2022-05-12 10:17:02 +0200

    Desktop version

--- src/components/HomeSimpleSteps.js
+++ src/components/HomeSimpleSteps.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import decoration from "../assets/Decoration.svg";
+import SimpleStepsItem from "./SimpleStepsItem";
 const HomeSimpleSteps = () => {
   return (
     <section id="simple-steps" className="simple-steps">
@@ -11,7 +12,28 @@ const HomeSimpleSteps = () => {
           src={decoration}
           alt="decoration-simple-steps"
         />
-        <div className="simple-steps-list"></div>
+        <div className="simple-steps-list">
+          <SimpleStepsItem
+            icon="Icon-1.svg"
+            title="Wybierz rzeczy"
+            description="ubrania, zabawki, sprzęt i inne"
+          />
+          <SimpleStepsItem
+            icon="Icon-2.svg"
+            title="Spakuj je"
+            description="skorzystaj z worków na śmieci"
+          />
+          <SimpleStepsItem
+            icon="Icon-3.svg"
+            title="Zdecyduj komu chcesz pomóc"
+            description="Zdecyduj komu chcesz pomóc"
+          />
+          <SimpleStepsItem
+            icon="Icon-4.svg"
+            title="Zamów kuriera"
+            description="kurier przyjedzie w dogodnym terminie"
+          />
+        </div>
         <Link to="/oddaj-rzeczy" className="simple-steps-btn">
           Oddam rzeczy
         </Link>
--- src/components/SimpleStepsItem.js
+++ src/components/SimpleStepsItem.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const SimpleStepsItem = ({ icon, title, description }) => {
   return (
-    <div>
+    <div className="simple-item">
       <img src={require(`../assets/${icon}`)} alt="simple-item-icon" />
       <h5 className="simple-item-title">{title}</h5>
       <p className="simple-item-description">{description}</p>
