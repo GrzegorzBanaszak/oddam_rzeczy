e27d8cbccf6ea359e472f5b521d03eec774acd04
GrzegorzBanaszak <grzegorz.banaszak95@gmail.com>
2022-05-16 12:23:19 +0200

    Layout login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,29 @@
 import React from "react";
-
+import Nav from "./Nav";
+import decoration from "../assets/Decoration.svg";
+import { Link } from "react-router-dom";
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <section className="login">
+      <Nav />
+      <div className="login-container">
+        <h2 className="login-header">Zaloguj się</h2>
+        <img className="login-decoration" src={decoration} alt="decoration" />
+        <form>
+          <div className="login-content">
+            <label htmlFor="email">Email</label>
+            <input type="text" name="email" />
+            <label htmlFor="passworld">Hasło</label>
+            <input type="passworld" name="passworld" />
+          </div>
+          <div className="login-btns">
+            <Link to="/rejstracja">Załóż konto</Link>
+            <button type="submit">Zaloguj się</button>
+          </div>
+        </form>
+      </div>
+    </section>
+  );
 };
 
 export default Login;
---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+const firebaseConfig = {
+  apiKey: "AIzaSyDwb4hMZz7R1ve0Dzk11WdmerKxwGjqPsQ",
+  authDomain: "oddam-rzeczy-cb8bf.firebaseapp.com",
+  projectId: "oddam-rzeczy-cb8bf",
+  storageBucket: "oddam-rzeczy-cb8bf.appspot.com",
+  messagingSenderId: "804420605346",
+  appId: "1:804420605346:web:5157513a1a89778e3d0efb",
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
