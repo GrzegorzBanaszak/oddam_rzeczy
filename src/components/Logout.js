c36b1dd7acd86526d0c95601cbc6a7012f872f9a
GrzegorzBanaszak <grzegorz.banaszak95@gmail.com>
2022-05-16 16:01:49 +0200

    Logout layout

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -1,7 +1,22 @@
 import React from "react";
-
+import Nav from "./Nav";
+import NavMobile from "./NavMobile";
+import decoration from "../assets/Decoration.svg";
+import { Link } from "react-router-dom";
 const Logout = () => {
-  return <div>Logout</div>;
+  return (
+    <section className="logout">
+      <Nav />
+      <NavMobile />
+      <div className="logout-container">
+        <h2 className="logout-header">Wylogowanie nastąpiło pomyślnie</h2>
+        <img className="logout-decoration" src={decoration} alt="decoration" />
+        <Link className="logout-link" to="/">
+          Strona główna
+        </Link>
+      </div>
+    </section>
+  );
 };
 
 export default Logout;
