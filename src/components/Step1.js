3f8b6f99c8f84ce8fa1ee6be9f6c5c73be8f466b
GrzegorzBanaszak <grzegorz.banaszak95@gmail.com>
2022-05-21 18:56:58 +0200

    Style for step 1

--- src/components/Step1.js
+++ src/components/Step1.js
@@ -1,7 +1,45 @@
 import React from "react";
+import StepsWaring from "./StepsWaring";
+import bg from "../assets/Background-Form.jpg";
 
 const Step1 = () => {
-  return <div>Step1</div>;
+  return (
+    <section className="steps">
+      <StepsWaring>
+        <h4>Ważne</h4>
+        <p>
+          Uzupełnij szczegóły dotyczące Twoich rzeczy. Dzięki temu będziemy
+          wiedzieć komu najlepiej je przekazać.
+        </p>
+      </StepsWaring>
+      <div
+        className="steps-container"
+        style={{ backgroundImage: `url(${bg})` }}
+      >
+        <div className="steps-content">
+          <h5>Krok 1/4</h5>
+          <h3>Zaznacz co chcesz oddać:</h3>
+          <div className="select-container">
+            <div className="select-element">
+              <span></span>ubrania, które nadają się do ponownego użycia
+            </div>
+            <div className="select-element">
+              <span></span>ubrania, do wyrzucenia
+            </div>
+            <div className="select-element">
+              <span></span>zabawki
+            </div>
+            <div className="select-element">
+              <span></span>książki
+            </div>
+            <div className="select-element">
+              <span></span>Inne
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Step1;
---
+++ src/components/StepsWaring.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const StepsWaring = ({ children }) => {
+  return (
+    <div className="steps-waring">
+      <div>{children}</div>
+    </div>
+  );
+};
+
+export default StepsWaring;
