2497175ce59b06d6cfb7dff2727f4b0e361b50c0
GrzegorzBanaszak <grzegorz.banaszak95@gmail.com>
2022-05-21 16:47:08 +0200

    Add form steps

--- src/App.js
+++ src/App.js
@@ -8,11 +8,16 @@ import Register from "./components/Register";
 import Logout from "./components/Logout";
 import { useState } from "react";
 import { createContext } from "react";
+import Step1 from "./components/Step1";
+import Step2 from "./components/Step2";
+import Step3 from "./components/Step3";
+import Step4 from "./components/Step4";
 
 export const DefaultContext = createContext();
 
 function App() {
   const [user, setUser] = useState(null);
+  const [formStep, setFormStep] = useState(1);
   return (
     <BrowserRouter>
       <DefaultContext.Provider value={{ user, setUser }}>
@@ -21,7 +26,48 @@ function App() {
           <Route
             path="/oddaj-rzeczy"
             element={user ? <GiveForm /> : <Navigate to="/logowanie" />}
-          />
+          >
+            <Route
+              index
+              element={
+                formStep === 1 ? (
+                  <Step1 />
+                ) : (
+                  <Navigate to={`/step-${formStep}`} />
+                )
+              }
+            />
+            <Route
+              path="/step-2"
+              element={
+                formStep === 2 ? (
+                  <Step2 />
+                ) : (
+                  <Navigate to={`/step-${formStep}`} />
+                )
+              }
+            />
+            <Route
+              path="/step-3"
+              element={
+                formStep === 3 ? (
+                  <Step3 />
+                ) : (
+                  <Navigate to={`/step-${formStep}`} />
+                )
+              }
+            />
+            <Route
+              path="/step-4"
+              element={
+                formStep === 4 ? (
+                  <Step4 />
+                ) : (
+                  <Navigate to={`/step-${formStep}`} />
+                )
+              }
+            />
+          </Route>
           <Route path="/logowanie" element={<Login />} />
           <Route path="/rejestracja" element={<Register />} />
           <Route path="/wylogowanie" element={<Logout />} />
