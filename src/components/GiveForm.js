eaa7f2227299aee96026ec925ce1bab6d2d190c2
GrzegorzBanaszak <grzegorz.banaszak95@gmail.com>
2022-05-22 13:12:57 +0200

    Style for step 4

--- src/components/GiveForm.js
+++ src/components/GiveForm.js
@@ -7,6 +7,7 @@ import Step1 from "../components/Step1";
 import Step2 from "../components/Step2";
 import Step3 from "../components/Step3";
 import Step4 from "../components/Step4";
+import HomeContact from "./HomeContact";
 
 const formValuesDef = {
   step1: "",
@@ -100,6 +101,7 @@ const GiveForm = () => {
         </div>
       </section>
       {renderStep()}
+      <HomeContact />
     </>
   );
 };
--- src/components/Step4.js
+++ src/components/Step4.js
@@ -1,7 +1,77 @@
 import React from "react";
-
-const Step4 = () => {
-  return <div>Step4</div>;
+import StepsWaring from "./StepsWaring";
+import bg from "../assets/Background-Form.jpg";
+import arrowDown from "../assets/Icon-Arrow-Down.svg";
+import arrowUp from "../assets/Icon-Arrow-Up.svg";
+import { useState } from "react";
+const Step4 = ({ formValues, setFormValues, setFormStep }) => {
+  return (
+    <section className="steps">
+      <StepsWaring>
+        <h4>Ważne</h4>
+        <p>
+          Uzupełnij szczegóły dotyczące Twoich rzeczy. Dzięki temu będziemy
+          wiedzieć komu najlepiej je przekazać.
+        </p>
+      </StepsWaring>
+      <div
+        className="steps-container"
+        style={{ backgroundImage: `url(${bg})` }}
+      >
+        <div className="steps-content">
+          <h5>Krok 4/4</h5>
+          <h3>Podaj adres oraz termin odbioru rzecz przez kuriera</h3>
+          <div className="select-container--4">
+            <div className="select-col">
+              <h4 className="select-subtitle">Adres odbioru:</h4>
+              <div className="steps-fild">
+                Ulica: <input type="text" />
+              </div>
+              <div className="steps-fild">
+                Miasto: <input type="text" />
+              </div>
+              <div className="steps-fild">
+                Kod pocztowy: <input type="text" />
+              </div>
+              <div className="steps-fild">
+                Numer telefonu: <input type="text" />
+              </div>
+            </div>
+            <div className="select-col">
+              <h4 className="select-subtitle">Termin odbioru:</h4>
+              <div className="steps-fild">
+                Data: <input type="text" />
+              </div>
+              <div className="steps-fild">
+                Godzina: <input type="text" />
+              </div>
+              <div className="steps-fild">
+                Uwagi dla kuriera: <textarea name="" cols="18" />
+              </div>
+            </div>
+          </div>
+          <div className="steps-control">
+            <div
+              to="step-2"
+              className="controler"
+              onClick={() => setFormStep(3)}
+            >
+              Wstecz
+            </div>
+            {formValues.step2 && (
+              <div
+                to="step-2"
+                className="controler"
+                onClick={() => setFormStep(5)}
+              >
+                Dalej
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Step4;
