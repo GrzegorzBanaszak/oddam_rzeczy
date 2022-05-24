cbc059493dd70f6d47b77094eb5efb8376f4944b
GrzegorzBanaszak <grzegorz.banaszak95@gmail.com>
2022-05-24 13:46:00 +0200

    Thangs step for give form

--- src/components/StepThanks.js
+++ src/components/StepThanks.js
@@ -1,7 +1,31 @@
 import React from "react";
-
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import bg from "../assets/Background-Form.jpg";
+import decoration from "../assets/Decoration.svg";
 const StepThanks = () => {
-  return <div>StepThanks</div>;
+  const nav = useNavigate();
+  useEffect(() => {
+    setTimeout(() => {
+      nav("/");
+    }, 5000);
+  }, []);
+  return (
+    <section className="steps">
+      <div
+        className="steps-container"
+        style={{ backgroundImage: `url(${bg})` }}
+      >
+        <div className="summery-thangs">
+          <h2>
+            Dziękujemy za przesłanie formularza Na maila prześlemy wszelkie
+            informacje o odbiorze.
+          </h2>
+          <img src={decoration} alt="decoration" />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default StepThanks;
